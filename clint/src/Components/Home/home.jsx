ff3f597efbb8bd2d760f58f3d0a7161e5737c774
Anoop Singh <anoop.gada@gmail.com>
2024-07-25 08:07:23 +0530

    initial render & update every minutes

--- clint/src/Components/Home/home.jsx
+++ clint/src/Components/Home/home.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Navbar from "../Navbar/navbar";
 import './home.css'
 import axios from 'axios';
@@ -7,38 +7,48 @@ const url = 'http://localhost:5000/stocks'
 
 function Home() {
     const [stockprice, setStockprice] = useState("Select stock");
-    const [stockname, setStockname] = useState("Stocks");
     let [stock, setdata] = useState([]);
     const [updateNum, setUpdateNum] = useState(0)
-
-    function getdata() {
-        var interval = setInterval(doStuff, 60000);
-        async function doStuff() {
-            let res = await axios.put(url);
-            setdata(res.data)
-
-        }
-    }
-
+    let useref = useRef("nocomp")
+    let interval;
     useEffect(() => {
-        getdata()
-        if (stock.length > 5) {
-            let price = stock.filter((ele) => {
-                return ele.company === company
-            })
-            if (price != undefined) {
-                console.log(price, "error")
-                setStockprice(price[0].price);
-                setUpdateNum(price[0].num)
-
+        async function rtstocks(com) {
+            let res = await axios.put(url);
+            setdata(res.data);
+            console.log(com)
+            if (com == undefined || com == "nocomp") {
+                setStockprice(res.data[0].price);
+                setUpdateNum(res.data[0].num);
             }
-
+            else {
+                let comp = res.data.filter((val, idx) => {
+                    return val.company === com
+                })
+                if (comp[0] !== undefined) {
+                    setStockprice(comp[0].price);
+                    setUpdateNum(comp[0].num);
+                }
+            }
         }
+        rtstocks()
+        interval = setInterval(() => {
+            let comp = useref.current;
+            rtstocks(comp);
+        }, 60000);
+        return () => clearInterval(interval);
+    }, [])
 
-    }, [stock])
-    const [company, setCompany] = useState('3M')
     function selectedStock(e) {
-        setCompany(e.target.value);
+        useref.current = e.target.value
+        for (let i = 0; i < stock.length; i++) {
+            if (stock[i].company === e.target.value) {
+                console.log(stock[i].price)
+                setStockprice(stock[i].price.toFixed(2));
+                setUpdateNum(stock[i].num)
+                break
+            }
+        }
+        clearInterval(interval)
 
     }
 
@@ -48,7 +58,7 @@ function Home() {
             <div className="stockcs-cont">
                 <div id="stockName">
                     <label> Choose Stocks :</label>
-                    <select onChange={(e) => { selectedStock(e) }}>
+                    <select onChange={selectedStock}>
                         {stock?.map((val, idx) => {
                             return (<>
                                 <option value={val.company}>{val.company}</option>
@@ -59,7 +69,7 @@ function Home() {
                 </div>
                 <div id="stockName">
                     <label> Current Price :</label><span style={{ color: `${updateNum < 0 ? "red" : "green"}` }} > {updateNum < 0 ? updateNum : "+" + updateNum} </span>
-                    <input type="text" readOnly="true" value={stockprice} />
+                    <input type="text" readOnly="true" value={stock.length === 0 ? "Select stock" : stockprice} />
 
                 </div>
             </div>
